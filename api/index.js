f84276e001323463fc3d3b01ace8e0bef6335a6d
Ricardoux7 <ricardosenior2006@gmail.com>
2025-12-14 17:11:17 -0400

    updated options

--- api/index.js
+++ api/index.js
@@ -40,7 +40,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
