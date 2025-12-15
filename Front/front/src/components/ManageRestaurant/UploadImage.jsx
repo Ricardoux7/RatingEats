0e17eae8030fe93fb153a416ce8684ea7aff5483
Ricardoux7 <ricardosenior2006@gmail.com>
2025-12-14 20:57:38 -0400

    test3

--- Front/front/src/components/ManageRestaurant/UploadImage.jsx
+++ Front/front/src/components/ManageRestaurant/UploadImage.jsx
@@ -63,9 +63,12 @@ const UploadImage = ({ restaurantId, imageId, mode = 'add', onUploadSuccess, onC
     try {
       let imageUrls = [];
       if (mode === 'add' || mode === 'replace' || mode === 'bannerUpload') {
-        console.log('TOKEN:', import.meta.env.VITE_BLOB_READ_WRITE_TOKEN);
         for (const file of selectedFiles) {
-          const { url } = await put(`images/${Date.now()}-${file.name}`, file, { access: 'public' });
+          const { url } = await put(
+            `images/${Date.now()}-${file.name}`,
+            file,
+            { access: 'public', token: import.meta.env.VITE_BLOB_READ_WRITE_TOKEN }
+          );
           imageUrls.push(url);
         }
       }
